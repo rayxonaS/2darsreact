5059aadae24cb9d728335f3b046c13f6f0445d0d
rayxona <rayxona@gmail.com>
2025-03-12 10:17:30 +0500

    implement title management with add and delete functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,64 @@
-function App() {
-  let a = 0;
+import { useState } from "react";
 
-  const increment = () => {
-    a++;
-    console.log(a);
+function App() {
+  const [text, setText] = useState("");
+  const [titles, setTitles] = useState([
+    {
+      title: "Title-1",
+      id: 1,
+    },
+    {
+      title: "Title-2",
+      id: 2,
+    },
+    {
+      title: "Title-3",
+      id: 3,
+    },
+  ]);
+
+  const deleteTitle = (id) => {
+    setTitles(titles.filter((t) => t.id !== id));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitles([
+      ...titles,
+      {
+        title: text,
+        id: Math.random(),
+      },
+    ]);
+    setText("");
+  };
+
   return (
     <div>
-      <form className="form">
-        <input className="input" type="text" />
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Write what do you want"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          value={text}
+        />
         <button className="button">Add</button>
       </form>
+      <ul>
+        {titles.map((t) => {
+          return (
+            <li key={t.id}>
+              <p>{t.title}</p>
+              <button onClick={() => deleteTitle(t.id)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
+
 export default App;
